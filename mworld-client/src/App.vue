ed97f02bfc0d1682b8ac2386212bef76be6f1d08
Unknown <noah.stride@selfsown.com>
2017-09-11 21:24:30 +0100

    this is better

---
+++ mworld-client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Nav'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style src="@/assets/main.less" lang="less">
---
+++ mworld-client/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1>NiG mWorld Bank!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non risus nibh. Etiam at ante at purus tincidunt faucibus eu at nibh. Nunc auctor nulla a sem porta, at sagittis nulla egestas. Ut odio tellus, varius ac pulvinar id, dictum et lacus. Nulla placerat nulla in orci laoreet, eu sollicitudin erat scelerisque. Fusce est sapien, rhoncus non risus vel, porttitor vehicula velit. Nam congue porta nisl in tincidunt. Nam at ante pulvinar, sollicitudin mi eu, tincidunt libero.</p>
+      <p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4">
+        <div class="panel panel-primary">
+          <div class="panel-heading">Panel heading without title</div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-warning">
+          <div class="panel-heading">Panel heading without title</div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-success">
+          <div class="panel-heading">Panel heading without title</div>
+          <div class="panel-body">
+            Panel content
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
