aceb09013f8173486ddcf2bc6431e87f5636a2c5
Unknown <noah.stride@selfsown.com>
2017-09-15 18:33:04 +0100

    Colors

--- mworld-client/src/components/Nav.vue
+++ mworld-client/src/components/Nav.vue
@@ -10,7 +10,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          <a class="navbar-brand" href="#">NiG mWorld Bank</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
