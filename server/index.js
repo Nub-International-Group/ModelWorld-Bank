190dd4a511480b196d5604929aecf5f4586948d3
Unknown <noah.stride@selfsown.com>
2017-09-16 13:42:55 +0100

    Health and 404

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+/**
+ * All responses should be JSON unless otherwise mentioned
+ */
+app.use(function (req, res, next) {
+  res.contentType('application/json')
+  next()
+})
+
 mongoose.connect('mongodb://127.0.0.1/mWorld')
 
 mongoose.connection.on('error', function (err) {
@@ -14,6 +22,24 @@ mongoose.connection.on('error', function (err) {
   console.log(err)
 
   process.exit()
+})
+
+/**
+ * Endpoints Begin
+ */
+
+/**
+ * Health Endpoint. Sends 200 if up
+ */
+app.get('/api/health', function (req, res, next) {
+  res.status(200).json({time: new Date(), uptime: process.uptime(), memory: process.memoryUsage()})
+})
+
+/**
+ * 404 Handler
+ */
+app.use(function (req, res, next) {
+  res.status(404).json({err: {code: 404, desc: 'Resource not found'}})
 })
 
 app.listen(process.env.PORT || 8081)
