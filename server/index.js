4d209ea3908c0b18dcfc688322a4d295490e7408
Unknown <noah.stride@selfsown.com>
2017-09-16 15:54:49 +0100

    Work

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json())
  */
 app.use(function (req, res, next) {
   res.contentType('application/json')
+  res.setHeader('X-Powered-By', 'The dreams and salt of MHOC')
   next()
 })
 
