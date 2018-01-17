5096a9179958b40a06f5319ed3474e68715aeae8
Unknown <noah.stride@selfsown.com>
2018-01-17 14:48:06 +0000

    nigal
    Closes #18

--- mworld-client/src/pages/PageNotFound.vue
+++ mworld-client/src/pages/PageNotFound.vue
@@ -15,7 +15,7 @@
     <br>
     <h3 class="text-center">This probably isn't the page you wanted. I'm sorry...</h3>
     <p class="text-center"><router-link to="/"><button type="button" class="btn btn-primary btn-lg ">Go Home</button></router-link></p>
-<div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/K6VhXtbgCXqQU" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/idk-shrug-shrugging-K6VhXtbgCXqQU">via GIPHY</a></p>
+    <img class="center-block img-responsive" src="http://www.yes-minister.com/images/oth_humphreycup.jpg" /></div>
   </div>
 </template>
 
