93fb1e6dda0182cfbc8ed53ac7740c0035a8b60f
Unknown <noah.stride@selfsown.com>
2018-02-26 17:32:38 +0000

    Betting

---
+++ server/models/bet.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const shortid = require('shortid') // Smarter, shorter IDs than the default MongoDB ones
+
+let betHistory = new mongoose.Schema({
+  time: { type: Date, default: Date.now },
+  odd: Number,
+  comment: String
+})
+
+let betOption = new mongoose.Schema({
+  _id: {type: String, default: shortid.generate},
+  name: String,
+  description: String,
+  currentOdds: Number,
+  historicalOdds: [ betHistory ]
+})
+
+let schema = new mongoose.Schema({
+  _id: {type: String, default: shortid.generate},
+  name: String,
+  description: String,
+  created: { type: Date, default: Date.now },
+  options: [ betOption ]
+}, { collection: 'bets' })
+
+let model = mongoose.model('wage', schema)
+
+module.exports = model
