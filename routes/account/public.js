57d648a526a993b1820a3bba0418af4d077a63f6
Unknown <noah.stride@selfsown.com>
2018-10-23 19:32:49 +0100

    API Adjustment

--- routes/account/public.js
+++ routes/account/public.js
@@ -10,39 +10,40 @@ module.exports = function (req, res) {
 }
 
 function updateLeaderboard () {
-  Account.find({public: true}).exec(function (err, accounts) {
+  Account.find({public: true}).exec((err, accounts) =>{
     if (err) {
       console.log(err)
     }
 
     const balances = {}
-    Transaction.find({}).exec(function (err, trans) {
+    Transaction.find({}).exec((err, trans) => {
       if (err) {
         console.log(err)
       }
 
-      trans.forEach(function (el) {
+      trans.forEach((el) => {
         if (!balances[el.to]) balances[el.to] = {}
         balances[el.to][el.currency] = (balances[el.to][el.currency] || 0) + el.amount
 
         if (!balances[el.from]) balances[el.from] = {}
         balances[el.from][el.currency] = (balances[el.from][el.currency] || 0) - el.amount
       })
-    })
+      leaderboard = []
 
-    leaderboard = []
-    accounts.forEach((acc) => {
-      let bal = 0
-      if (balances[acc._id]) bal = balances[acc._id]['GBP'] || 0
+      accounts.forEach((acc) => {
+        let bal = 0
+        if (balances[acc._id]) bal = balances[acc._id]['GBP'] || 0
 
-      leaderboard.push({
-        name: acc.name,
-        description: acc.description,
-        balance: bal
+        leaderboard.push({
+          name: acc.name,
+          description: acc.description,
+          balance: bal
+        })
+
+        lastUpdated = Date.now()
       })
-
-      lastUpdated = Date.now()
     })
+
   })
 }
 
