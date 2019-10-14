3a03133e91de4e270a467ded0e484f300ebf1bd5
Noah Stride <noah@noahstride.co.uk>
2019-10-14 18:00:54 +0100

    Async await for transaction creation

--- routes/account/transaction/new.js
+++ routes/account/transaction/new.js
@@ -1,72 +1,84 @@
 const Account = require('../../../models/account.js')
 const Transaction = require('../../../models/transaction.js')
 
-module.exports = function (req, res, next) {
-  Account.findOne({'_id': req.params.id}, function (err, sendingAccount) {
-    if (err) {
-      return next(err)
-    }
+module.exports = async function (req, res, next) {
+  try {
+    const sendingAccount = await Account.findOne({ '_id': req.params.id }).exec()
 
     if (sendingAccount == null) {
-      return res.status(404).json({err: {code: 404, desc: 'Resource not found'}})
+      const err = new Error('Sending account not found')
+      err.code = 404
+
+      throw err
     }
 
     req.body.currency = 'GBP'
     if (['GBP', 'USD'].includes(req.body.currency) === false) {
-      return res.status(500).json({err: {code: 500, desc: 'Invalid currency'}})
+      const err = new Error('Invalid currency specified')
+      err.code = 422
+
+      throw err
     }
 
     if ((sendingAccount.users[req.decoded.name] === 3) || (req.decoded.admin === true)) { // Permission level greater than 1 or they are admin
-      Account.findOne({'_id': req.body.target}, function (err, targetAccount) {
-        if (err) {
-          return next(err)
+      const targetAccount = await Account.findOne({ '_id': req.body.target }).exec()
+      if (sendingAccount == null) {
+        const err = new Error('Target account not found')
+        err.code = 404
+
+        throw err
+      }
+
+      let amount = Number(req.body.amount)
+
+      if (isNaN(amount)) {
+        const err = new Error('Invalid amount')
+        err.code = 422
+
+        throw err
+      }
+
+      if (amount <= 0) {
+        const err = new Error('Cannot send negative')
+        err.code = 422
+
+        throw err
+      }
+
+      const data = await sendingAccount.calculateBalance()
+      if (data.balance[req.body.currency] || sendingAccount._id === '*economy*') {
+        if (data.balance[req.body.currency] >= (amount + 10) || sendingAccount._id === '*economy*') {
+          let transactions = [{
+            from: sendingAccount._id,
+            to: targetAccount._id,
+            amount: amount,
+            currency: req.body.currency,
+            description: req.body.description,
+            authoriser: req.decoded.name
+          }, {
+            from: sendingAccount._id,
+            to: 'SJS4nVQLG',
+            amount: 10,
+            currency: req.body.currency,
+            description: 'Tx Fee',
+            authoriser: req.decoded.name
+          }]
+
+          await Transaction.insertMany(transactions)
+          return res.status(200).json({})
+        } else {
+          throw new Error('Not enough balance')
         }
-
-        if (sendingAccount == null) {
-          return res.status(404).json({err: {code: 404, desc: 'Target not found'}})
-        }
-
-        let amount = Number(req.body.amount)
-
-        if (isNaN(amount)) {
-          return res.status(500).json({err: {code: 500, desc: 'Invalid amount'}})
-        }
-
-        if (amount <= 0) {
-          return res.status(500).json({err: {code: 500, desc: 'Don\'t be silly'}})
-        }
-
-        sendingAccount.calculateBalance().then((data) => {
-          if (data.balance[req.body.currency] || sendingAccount._id === '*economy*') {
-            if (data.balance[req.body.currency] >= (amount + 10) || sendingAccount._id === '*economy*') {
-              let transactions = [{
-                from: sendingAccount._id,
-                to: targetAccount._id,
-                amount: amount,
-                currency: req.body.currency,
-                description: req.body.description,
-                authoriser: req.decoded.name
-              }, {
-                from: sendingAccount._id,
-                to: 'SJS4nVQLG',
-                amount: 10,
-                currency: req.body.currency,
-                description: 'Tx Fee',
-                authoriser: req.decoded.name
-              }]
-
-
-              return Transaction.insertMany(transactions)
-            } else {
-              throw new Error('Not enough balance')
-            }
-          } else {
-            throw new Error('No balance in currency')
-          }
-        }).then(() => res.status(200).json({})).catch(next)
-      })
+      } else {
+        throw new Error('No balance in currency')
+      }
     } else {
-      return res.status(403).json({err: {code: 403, desc: 'You do not have permission'}})
+      const err = new Error('You don\'t have permission to transfer from this account')
+      err.code = 403
+
+      throw err
     }
-  })
+  } catch (err) {
+    next(err)
+  }
 }
