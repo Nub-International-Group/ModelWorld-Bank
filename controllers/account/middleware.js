da7d4444086023abaccbbaebf289a1bba3641904
Noah Stride <noah@noahstride.co.uk>
2019-11-10 17:11:05 +0000

    Cleaner  404 handling

--- controllers/account/middleware.js
+++ controllers/account/middleware.js
@@ -3,13 +3,6 @@ const accountWithPerms = (permLevel) => {
     try {
       const account = req.account
 
-      if (!account) {
-        const e = new Error('account does not exist')
-        e.code = 404
-
-        throw e
-      }
-
       if (permLevel) {
         if (!(account.users[req.decoded.name] >= permLevel || req.decoded.admin === true)) {
           const e = new Error('You do not have permission to access this account')
