9f235f30cc0383a56a317c27e6f8758292e72ebf
Noah Stride <noah@noahstride.co.uk>
2019-12-02 17:43:02 +0000

    fix payment job stuff

---
+++ payment-cron.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const logger = require('pino')()
+const Account = require('./models/account')
+
+mongoose.connect(config.mongoURL)
+mongoose.Promise = Promise
+
+mongoose.connection.on('error', function (err) {
+  logger.fatal(err, 'error connecting to mongodb')
+
+  process.exit(1)
+})
+
+const payAccounts = async () => {
+  const accounts = await Account.find({ company: false }).populate('wages').exec()
+  logger.info(`${accounts.length} accounts to process wages for.`)
+
+  const promises = accounts.map(account => {
+    return new Promise((resolve, reject) => {
+      account.payWages((err) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  })
+
+  return Promise.all(promises)
+}
+
+logger.info('starting account payment cron job')
+payAccounts()
+  .then(() => {
+    logger.info('payment cron job completed')
+    process.exit(0)
+  })
+  .catch(err => {
+    logger.fatal(err, 'an error occured completing the payment cron job')
+    process.exit(1)
+  })
