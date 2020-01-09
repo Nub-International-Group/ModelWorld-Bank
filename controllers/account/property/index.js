d80bc8668ff59cd146df5a584626c2f429bb81c7
Noah Stride <noah@noahstride.co.uk>
2020-01-09 17:37:52 +0000

    API support for property sales

--- controllers/account/property/index.js
+++ controllers/account/property/index.js
@@ -2,6 +2,7 @@ const router = (require('express')).Router()
 
 const Property = require('../../../models/property')
 
+const utils = require('../../../utils')
 const middleware = require('../middleware')
 
 async function find (req, res, next) {
@@ -14,8 +15,45 @@ async function find (req, res, next) {
   }
 }
 
-router.get('/', middleware.accountWithPerms(), find)
+async function sell ({ body, property, decoded: user }, res, next) {
+  try {
+    if (!body.accountId) {
+      const e = new Error('accountId must be specified.')
+      e.code = 422
 
+      throw e
+    }
+
+    let value = body.value
+    if (typeof value !== 'number' || value < 0) {
+      const e = new Error('A valid positive numerical value must be provided for the sale')
+      e.code = 422
+
+      throw e
+    }
+    value = Number(value.toFixed(2))
+
+    const updated = await property.update({
+      owner: body.accountId,
+      valuations: [
+        ...property.valuations || [],
+        {
+          created: Date.now(),
+          user: user.name
+        }
+      ]
+    }).exec()
+
+    res.status(200).json(updated)
+  } catch (e) {
+    next(e)
+  }
+}
+
+router.get('/', middleware.accountWithPerms(1), find)
+router.post('/sell', middleware.accountWithPerms(2), sell)
+
+router.param('propertyId', utils.generateParamMiddleware(Property, 'property'))
 module.exports = {
   router
 }
--- utils.js
+++ utils.js
@@ -1,4 +1,14 @@
-const generateParamMiddleware = (Model, name) => {
+function pickProperties (obj, keys) {
+  const filtered = {}
+
+  for (const key of keys) {
+    filtered[key] = obj[key]
+  }
+
+  return filtered
+}
+
+function generateParamMiddleware (Model, name) {
   return async (req, res, next) => {
     try {
       const doc = await Model.findOne({ _id: req.params[name + 'Id'] }).exec()
@@ -18,6 +28,24 @@ const generateParamMiddleware = (Model, name) => {
   }
 }
 
+function generatePatchHandler (modelName, attributes) {
+  return async (req, res, next) => {
+    try {
+      const filtered = pickProperties(req[modelName], attributes)
+
+      if (!Object.keys(filtered).length) {
+        return res.status(204).send(req[modelName])
+      }
+
+      const updated = await req[modelName].update(filtered).exec()
+      res.status(200).json(updated)
+    } catch (e) {
+      next(e)
+    }
+  }
+}
+
 module.exports = {
-  generateParamMiddleware
+  generateParamMiddleware,
+  generatePatchHandler
 }
