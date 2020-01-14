1f59102afbc152fa2d0483e2c8b2a2e3f9098092
Noah Stride <noah@noahstride.co.uk>
2020-01-14 10:53:40 +0000

    fix(server): wage _id

--- packages/server/controllers/wage/index.js
+++ packages/server/controllers/wage/index.js
@@ -19,6 +19,7 @@ const deleteById = async (req, res, next) => {
 
 const create = async (req, res, next) => {
   try {
+    delete req.body._id
     const wage = await Wage.create(req.body)
     res.status(200).json(wage)
   } catch (err) {
